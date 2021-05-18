4396aa624deed72a23c1652f9fc09c7e98b5b126
default <tinvs2021@protonmail.com>
2021-05-18 18:15:46 +0200

    commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xc3cabc1f565c88bdd416cfd65379f69e4ded649b',
+    97: '',
+  },
+  masterChef: {
+    56: '0x60E2D2732bfB34a20ed931C200eBc86F6516b373',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
